d0a8e01353fcb8c079d31ae91e4e72d31d937d86
Dalwar Hossain <dalwar.hossain@protonmail.com>
2018-10-25 16:38:01 +0200

    updated questions for both type of flights

--- jetburn/_questions.py
+++ jetburn/_questions.py
@@ -4,14 +4,9 @@
 from __future__ import print_function, unicode_literals
 
 # Import python libraries
-import sys
-import inquirer
+from pprint import pprint
 import re
-import platform
 from PyInquirer import Validator, ValidationError
-
-# Import local python libraries
-import _icons as icon
 
 # Source code meta data
 __author__ = 'Dalwar Hossain'
@@ -30,7 +25,13 @@ class IataCodeValidator(Validator):
         pass
 
     def validate(self, document):
-        pass
+        ok = re.match('^[a-zA-Z]{3}$', document.text)
+        if not ok:
+            raise ValidationError(
+                message='Please use 3 letter IATA airport codes. To find out IATA codes '
+                        'use: jetburn --find-airport <city_name>',
+                cursor_position=len(document.text)
+            )
 
 
 # Date validator
@@ -39,7 +40,12 @@ class DateValidator(Validator):
         pass
 
     def validate(self, document):
-        pass
+        ok = re.match(date_reg_ex, document.text)
+        if not ok:
+            raise ValidationError(
+                message='Please enter date in correct format',
+                cursor_position=len(document.text)  # Moves cursor to the end
+            )
 
 
 class NumberValidator(Validator):
@@ -52,100 +58,112 @@ class NumberValidator(Validator):
         except ValueError:
             raise ValidationError(
                 message='Please enter a number',
-                cursor_position=len(document.text))
+                cursor_position=len(document.text)
+            )
 
 
-if platform.system() == "Windows":
-    # WINDOWS
-    # Generate questions for flight search information
-    # ----------------------- Round Trip ---------------------------------------------------------------
-    windows_questions = [
-        {
-            'type': 'input',
-            'name': 'origin',
-            'message': 'Origin airport:',
-            'validate': IataCodeValidator
-        },
-        {
-            'type': 'input',
-            'name': 'destination',
-            'message': 'Destination airport:',
-            'validate': IataCodeValidator
-        },
-        {
-            'type': 'input',
-            'name': 'fly_out_date',
-            'message': 'Fly out date (dd/mm/yyyy):',
-            'validate': DateValidator
-        },
-        {
-            'type': 'input',
-            'name': 'adults',
-            'message': 'Adults (>16 Years)?',
-            'validate': NumberValidator,
-            'filter': lambda val: int(val),
-            'default': 1
-        },
-        {
-            'type': 'input',
-            'name': 'teens',
-            'message': 'Teens (12-15 Years)?',
-            'validate': NumberValidator,
-            'filter': lambda val: int(val),
-            'default': 0
-        },
-        {
-            'type': 'input',
-            'name': 'children',
-            'message': 'Children (2-11 Years)?',
-            'validate': NumberValidator,
-            'filter': lambda val: int(val),
-            'default': 0
-        },
-        {
-            'type': 'input',
-            'name': 'infants',
-            'message': 'Infants (<2 Years)?',
-            'validate': NumberValidator,
-            'filter': lambda val: int(val),
-            'default': 0
-        }
-    ]
-    fly_back_question = {
-            'type': 'input',
-            'name': 'fly_back_date',
-            'message': 'Fly back date (dd/mm/yyyy):',
-            'validate': DateValidator
-        }
-    windows_round_trip_questions = windows_questions.insert(3, fly_back_question)
-else:
-    # LINUX/MAC
-    # Generate questions for flight search information
-    # -------------------- Round Trip ---------------------------------------------------------------
-    linux_round_trip_questions = [
-        inquirer.Text(name='origin', message='Origin airport ' + icon.departure,
-                      validate=lambda _, x: re.match('^[a-zA-Z]{3}$', x)),
-        inquirer.Text(name='destination', message='Destination airport ' + icon.arrival,
-                      validate=lambda _, x: re.match('^[a-zA-Z]{3}$', x)),
-        inquirer.Text(name='fly_out_date', message='Fly out date (dd/mm/yyyy) ' + icon.calendar,
-                      validate=lambda _, x: re.match(date_reg_ex, x)),
-        inquirer.Text(name='fly_back_date', message='Fly back date (dd/mm/yyyy) ' + icon.calendar,
-                      validate=lambda _, x: re.match(date_reg_ex, x)),
-        inquirer.Text(name='adults', message='Adults (>16 Years)? ' + icon.adult, default='1'),
-        inquirer.Text(name='teens', message='Teens (12-15 Years)? ' + icon.teen, default='0'),
-        inquirer.Text(name='children', message='Children (2-11 Years)? ' + icon.child, default='0'),
-        inquirer.Text(name='infants', message='Infants (<2 Years)? ' + icon.infant, default='0'),
-    ]
-    # -------------------- One Way --------------------------------------------------------------------
-    linux_one_way_questions = [
-        inquirer.Text(name='origin', message='Origin airport ' + icon.departure,
-                      validate=lambda _, x: re.match('^[a-zA-Z]{3}$', x)),
-        inquirer.Text(name='destination', message='Destination airport ' + icon.arrival,
-                      validate=lambda _, x: re.match('^[a-zA-Z]{3}$', x)),
-        inquirer.Text(name='fly_out_date', message='Fly out date (dd/mm/yyyy) ' + icon.calendar,
-                      validate=lambda _, x: re.match(date_reg_ex, x)),
-        inquirer.Text(name='adults', message='Adults (>16 Years)? ' + icon.adult, default='1'),
-        inquirer.Text(name='teens', message='Teens (12-15 Years)? ' + icon.teen, default='0'),
-        inquirer.Text(name='children', message='Children (2-11 Years)? ' + icon.child, default='0'),
-        inquirer.Text(name='infants', message='Infants (<2 Years)? ' + icon.infant, default='0'),
-    ]
+# Generate questions for flight search information
+one_way_questions = [
+    {
+        'type': 'input',
+        'name': 'origin',
+        'message': 'Origin airport:',
+        'validate': IataCodeValidator
+    },
+    {
+        'type': 'input',
+        'name': 'destination',
+        'message': 'Destination airport:',
+        'validate': IataCodeValidator
+    },
+    {
+        'type': 'input',
+        'name': 'fly_out_date',
+        'message': 'Fly out date (dd/mm/yyyy):',
+        'validate': DateValidator
+    },
+    {
+        'type': 'input',
+        'name': 'adults',
+        'message': 'Adults (>16 Years)?',
+        'validate': NumberValidator,
+        'filter': lambda val: int(val),
+        'default': '1'
+    },
+    {
+        'type': 'input',
+        'name': 'teens',
+        'message': 'Teens (12-15 Years)?',
+        'validate': NumberValidator,
+        'filter': lambda val: int(val),
+        'default': '0'
+    },
+    {
+        'type': 'input',
+        'name': 'children',
+        'message': 'Children (2-11 Years)?',
+        'validate': NumberValidator,
+        'filter': lambda val: int(val),
+        'default': '0'
+    },
+    {
+        'type': 'input',
+        'name': 'infants',
+        'message': 'Infants (<2 Years)?',
+        'validate': NumberValidator,
+        'filter': lambda val: int(val),
+        'default': '0'
+    }
+]
+fly_back_question = {
+        'type': 'input',
+        'name': 'fly_back_date',
+        'message': 'Fly back date (dd/mm/yyyy):',
+        'validate': DateValidator
+}
+
+
+def __get_questions(round_trip=None):
+    """
+    This function returns a set of questions based on trip type
+
+    :param round_trip: (boolean) If it's a round trip or not
+    :return: (list) python list of dictionary based questions
+    """
+    if round_trip:
+        one_way_questions.insert(3, fly_back_question)
+        round_trip_questions = one_way_questions
+        return round_trip_questions
+    else:
+        return one_way_questions
+
+
+# # Generate questions for flight search information
+# # -------------------- Round Trip ---------------------------------------------------------------
+# linux_round_trip_questions = [
+#     inquirer.Text(name='origin', message='Origin airport ' + icon.departure,
+#                   validate=lambda _, x: re.match('^[a-zA-Z]{3}$', x)),
+#     inquirer.Text(name='destination', message='Destination airport ' + icon.arrival,
+#                   validate=lambda _, x: re.match('^[a-zA-Z]{3}$', x)),
+#     inquirer.Text(name='fly_out_date', message='Fly out date (dd/mm/yyyy) ' + icon.calendar,
+#                   validate=lambda _, x: re.match(date_reg_ex, x)),
+#     inquirer.Text(name='fly_back_date', message='Fly back date (dd/mm/yyyy) ' + icon.calendar,
+#                   validate=lambda _, x: re.match(date_reg_ex, x)),
+#     inquirer.Text(name='adults', message='Adults (>16 Years)? ' + icon.adult, default='1'),
+#     inquirer.Text(name='teens', message='Teens (12-15 Years)? ' + icon.teen, default='0'),
+#     inquirer.Text(name='children', message='Children (2-11 Years)? ' + icon.child, default='0'),
+#     inquirer.Text(name='infants', message='Infants (<2 Years)? ' + icon.infant, default='0'),
+# ]
+# # -------------------- One Way --------------------------------------------------------------------
+# linux_one_way_questions = [
+#     inquirer.Text(name='origin', message='Origin airport ' + icon.departure,
+#                   validate=lambda _, x: re.match('^[a-zA-Z]{3}$', x)),
+#     inquirer.Text(name='destination', message='Destination airport ' + icon.arrival,
+#                   validate=lambda _, x: re.match('^[a-zA-Z]{3}$', x)),
+#     inquirer.Text(name='fly_out_date', message='Fly out date (dd/mm/yyyy) ' + icon.calendar,
+#                   validate=lambda _, x: re.match(date_reg_ex, x)),
+#     inquirer.Text(name='adults', message='Adults (>16 Years)? ' + icon.adult, default='1'),
+#     inquirer.Text(name='teens', message='Teens (12-15 Years)? ' + icon.teen, default='0'),
+#     inquirer.Text(name='children', message='Children (2-11 Years)? ' + icon.child, default='0'),
+#     inquirer.Text(name='infants', message='Infants (<2 Years)? ' + icon.infant, default='0'),
+# ]
