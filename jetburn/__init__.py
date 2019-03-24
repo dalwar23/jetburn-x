e34e2dd3d1bd93d1f2e39b4312565ab69b0cc057
dharif23 <dalwar.hossain@protonmail.com>
2019-03-24 11:35:52 +0100

    import handels updated

--- jetburn/__init__.py
+++ jetburn/__init__.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 """
-This package extracts flights data from different websites for fare comparison
+This package extracts flights information from different websites and shows ticket prices and other relative
+information.
 """
 
 # Source code meta data
 __author__ = 'Dalwar Hossain'
 __email__ = 'dalwar.hossain@protonmail.com'
 
-
-# Handle imports
-from _release_info import __release__, __version__, __author__, __email__
+# Handle Imports
+from ._version import __author__, __author_email__, __full_version__, __short_version__, __app_license__
