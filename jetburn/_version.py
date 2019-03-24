ea675c22f618a59e7ae393de1f229180c1acefd7
dharif23 <dalwar.hossain@protonmail.com>
2019-03-24 11:27:43 +0100

    changing the name of version file

---
+++ jetburn/_version.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Source code meta data
+__package_name__ = 'jetburn'
+__author__ = 'Dalwar Hossain'
+__author_email__ = "dalwar.hossain@protonmail.com"
+__major_version__ = "2"
+__release__ = "1"
+__patch__ = "0"
+__short_version__ = __major_version__ + "." + __release__
+__full_version__ = __major_version__ + "." + __release__ + "." + __patch__
+__app_license__ = "LGPL-3.0"
